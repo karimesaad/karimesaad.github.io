030a0e9f7db9298ab805df14281fab390be2ba57
Karime Saad <karime.saad@utexas.edu>
2017-02-22 15:01:56 -0600

    added responsiveness and animate.css

---
+++ js/cutom.js
@@ -0,0 +1,81 @@
+//add smooth scrolling
+$(document).ready(function() {
+    
+    'use strict';
+   
+    
+    $('.nav-item, #scroll-to-top').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+    
+    
+});
+
+/* active menu item on click */
+$(document).ready(function() {
+    
+    'use strict';
+    
+    
+    $('.navbar-nav li a').click(function(){
+        
+        'use strict';
+        
+        $('.navbar-nav li a').parent().removeClass("active");
+        
+        $(this).parent().addClass("active");
+    
+    });
+});
+
+// highlight menu item on scroll
+$(document).ready(function() {
+    
+    'use strict';
+    
+    $(window).scroll( function() {
+        
+        'use strict';
+        
+        $("section").each(function()  {
+            
+            'use strict';
+            
+            var bb = $(this).attr("id");  // ABOUT, CONTACT, DOWNLOAD
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+            
+            if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+             
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+           
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+                
+            }
+            
+            
+        });
+    
+    
+    });
+    
+    
+});
+
+$(document).ready(function(){
+
+	'use strict';
+
+	new WOW().init();
+
+});
